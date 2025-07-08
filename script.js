e89606cab160df97b7acdefd816b1f8529d0ba34
wandersonmat <wanderson.matheus@hotmail.com>
2025-07-07 21:08:49 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function mostrarMensagem() {
+    const mensagemDiv = document.getElementById("mensagem");
+    mensagemDiv.innerText = "Você é capaz de alcançar todos os seus objetivos!";
+    mensagemDiv.style.marginTop = "15px";
+    mensagemDiv.style.fontWeight = "bold";
+    mensagemDiv.style.color = "#2e8b57";
+}
